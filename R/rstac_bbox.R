656a2361c0a88ff57adad6489175d039f4e73059
Hugh Graham <hugh.a.graham@gmail.com>
2024-10-11 23:28:16 +0100

    fix docs

--- R/rstac_bbox.R
+++ R/rstac_bbox.R
@@ -11,7 +11,7 @@
 #' @examples
 #' rstac_bbox(c(-121.772, 45.321, -121.611, 45.419))
 #'
-#' #' if (requireNamespace("sf", quietly = TRUE)) {
+#' if (requireNamespace("sf", quietly = TRUE)) {
 #'   nc_sf <- system.file("gpkg", "nc.gpkg", package = "sf") |>
 #'     sf::read_sf()
 #'   rstac_bbox(nc_sf)
